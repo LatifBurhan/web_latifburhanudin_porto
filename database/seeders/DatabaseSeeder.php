8edde51899dc13364f9b98c35858a7886350e8c9
kotepgans <echo.kotepganzz2006@gmail.com>
2026-01-20 18:49:59 +0700

    admin@latif.com password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; // <--- PENTING: Jangan lupa ini
+use Illuminate\Support\Facades\Hash; // <--- PENTING: Buat enkripsi password
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // Kode ini akan Mencari email 'admin@latif.com'
+        // Kalau GAK ADA -> Dia buat baru
+        // Kalau SUDAH ADA -> Dia akan update password-nya jadi 'password'
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@latif.com'], // Cek berdasarkan email ini
+            [
+                'name' => 'Latif Admin',
+                'password' => Hash::make('password'), // Password default
+                'email_verified_at' => now(),
+            ]
+        );
+
+        // Bisa tambahkan seeder lain disini (Project, Skill, dll)
     }
 }
